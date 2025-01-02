0f4f22a49a9d96ea198ecd0fc9b150d8d649478f
Drilio <Roy-Antoine93@proton.me>
2025-01-02 13:52:16 +0100

    add fr trad

--- src/locales.js
+++ src/locales.js
@@ -2,11 +2,16 @@ export default {
   de: {
     manifest_remove: 'Ressource entfernen',
     window_add: 'In Galerieansicht einblenden',
-    window_remove: 'In Galerieansicht ausblenden'
+    window_remove: 'In Galerieansicht ausblenden',
   },
   en: {
     manifest_remove: 'Remove resource',
     window_add: 'Add to workspace',
-    window_remove: 'Remove from workspace'
+    window_remove: 'Remove from workspace',
   },
-}
+  fr: {
+    manifest_remove: 'Supprimer la ressource',
+    window_add: 'Ajouter à la vue de la galerie',
+    window_remove: 'Retirer de la vue de la galerie',
+  },
+};
