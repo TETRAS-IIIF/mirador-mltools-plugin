4dcb3e2bd7dfedd1bfd0a036a7775efa25033164
Anthony Geourjon <anthony.geourjon@tetras-libre.fr>
2025-10-15 10:46:00 +0200

    Publish demo from Github

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # mirador-mltools-plugin
 
-[![npm package][npm-badge]][npm]
-
 `mirador-mltools-plugin` is a [Mirador 4](https://github.com/projectmirador/mirador) plugin that adds tools to the manifest list displayed when `isWorkspaceAddVisible = true`. Users can remove an added manifest from the viewer by pressing the `Remove resource` button. Another feature is to open and close a manifest in the gallery view without switching views. 
 ## Installing `mirador-mltools-plugin`
 `mirador-mltools-plugin` requires an instance of Mirador 4. See the [Mirador wiki](https://github.com/ProjectMirador/mirador/wiki) for examples of embedding Mirador within an application. See the [demo's index.js](https://github.com/slub/mirador-mltools-plugin/blob/main/demo/src/index.js) for an example of importing the `mirador-mltools-plugin`.
@@ -14,3 +12,7 @@ This repo is a fork from https://github.com/slub/mirador-mltools-plugin
 
 We hae submitted unaccepted PR to the original repo. Original repo is not maintained anymore.
 We also add some features to the plugin to support Mirador Multi User environment. See https://github.com/SCENE-CE/mirador-multi-user
+
+# Demo
+
+A demo is available on Github and are redeployed automatically from the `main` branch.
--- vite.config.js
+++ vite.config.js
@@ -1,63 +1,76 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import fs from 'fs/promises';
-import { fileURLToPath } from 'url';
-import pkg from './package.json';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import fs from "fs/promises";
+import { fileURLToPath } from "url";
+import pkg from "./package.json";
 
 /**
-* Vite configuration
-*/
+ * Vite configuration
+ */
 export default defineConfig({
-
-  build: {
-    lib: {
-      entry: './src/index.js',
-      fileName: (format) => (format === 'umd' ? 'mirador-mltools-plugin.js' : 'mirador-mltools-plugin.es.js'),
-      formats: ['es', 'umd'],
-      name: 'MiradorMLToolsPlugin',
-    },
-    rollupOptions: {
-      external: [...Object.keys(pkg.peerDependencies || {}), '__tests__/*', '__mocks__/*'],
-      output: {
-        assetFileNames: 'mirador-mltools-plugin.[ext]',
-      },
-    },
-    sourcemap: true,
-  },
-  esbuild: {
-    exclude: [],
-    // Matches .js and .jsx in __tests__ and .jsx in src
-    include: [/__tests__\/.*\.(js|jsx)$/, /src\/.*\.jsx?$/],
-    loader: 'jsx',
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      plugins: [
-        {
-          name: 'load-js-files-as-jsx',
-          // TODO: rename all our files to .jsx ...
-
-          setup(build) {
-            build.onLoad({ filter: /(src|__tests__)\/.*\.js$/ }, async (args) => ({
-              contents: await fs.readFile(args.path, 'utf8'),
-              loader: 'jsx',
-            }));
-          },
+  base: process.env.GITHUB_PAGES ? (process.env.BASE_PATH || "/mirador-mltools-plugin/") : "/",
+  ...(
+    process.env.GITHUB_PAGES ? {
+      build: {
+        outDir: "dist",
+        emptyOutDir: true,
+        rollupOptions: {
+          external: ["__tests__/*", "__mocks__/*"],
+          input: fileURLToPath(new URL("./demo/src/index.html", import.meta.url))
         },
-      ],
-    },
-    include: [
-      '@emotion/react',
-    ],
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@tests/': fileURLToPath(new URL('./__tests__', import.meta.url)),
-    },
-  },
-  server: {
-    open: '/demo/src/index.html',
-    port: '4444',
-  },
+        sourcemap: true
+      }
+    } : {
+      build: {
+        lib: {
+          entry: "./src/index.js",
+          fileName: (format) => (format === "umd" ? "mirador-mltools-plugin.js" : "mirador-mltools-plugin.es.js"),
+          formats: ["es", "umd"],
+          name: "MiradorMLToolsPlugin"
+        },
+        rollupOptions: {
+          external: [...Object.keys(pkg.peerDependencies || {}), "__tests__/*", "__mocks__/*"],
+          output: {
+            assetFileNames: "mirador-mltools-plugin.[ext]"
+          }
+        },
+        sourcemap: true
+      },
+      esbuild: {
+        exclude: [],
+        // Matches .js and .jsx in __tests__ and .jsx in src
+        include: [/__tests__\/.*\.(js|jsx)$/, /src\/.*\.jsx?$/],
+        loader: "jsx"
+      },
+      optimizeDeps: {
+        esbuildOptions: {
+          plugins: [
+            {
+              name: "load-js-files-as-jsx",
+              // TODO: rename all our files to .jsx ...
+
+              setup(build) {
+                build.onLoad({ filter: /(src|__tests__)\/.*\.js$/ }, async (args) => ({
+                  contents: await fs.readFile(args.path, "utf8"),
+                  loader: "jsx"
+                }));
+              }
+            }
+          ]
+        },
+        include: [
+          "@emotion/react"
+        ]
+      },
+      plugins: [react()],
+      resolve: {
+        alias: {
+          "@tests/": fileURLToPath(new URL("./__tests__", import.meta.url))
+        }
+      },
+      server: {
+        open: "/demo/src/index.html",
+        port: "4444"
+      },
+    })
 });
